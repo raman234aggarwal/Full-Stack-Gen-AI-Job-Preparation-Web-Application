c3662c7c0c06f59fdca0ac90abf02fb21748cf5e
Raman Aggarwal <raman234aggarwal@gmail.com>
2026-07-04 19:01:47 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,10 +6,12 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
